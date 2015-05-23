dfa1fe7d57b6b4e45bf06e105c9061917fb6fc18
Shen Yichen <2007.yichen@gmail.com>
2015-05-23 23:41:47 +0800

    Center grid within panel
     - Change font size to calculate only if board has columns/rows
     - Fixed small bug where 0 rows were allowed
     - Comment on code and collate variables
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/battleships/gui/HighSeas.java
+++ src/battleships/gui/HighSeas.java
@@ -7,7 +7,6 @@ package battleships.gui;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
-import java.util.Stack;
 import javax.swing.JPanel;
 
 /**
@@ -28,7 +27,7 @@ public class HighSeas extends JPanel {
     }
 
     public void setSquares(int x, int y) {
-        if (x < 1 || y < 0) {
+        if (x < 1 || y < 1) {
             throw new IllegalArgumentException("There must be as least 1 row/column!");
         }
 
@@ -50,43 +49,60 @@ public class HighSeas extends JPanel {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        int maxDigits = (int) Math.max(Math.floor(Math.log10(xSqrs)) + 1, Math.floor(Math.log(ySqrs - 1) / Math.log(26) + 1));
-
-        double maxBound = g2.getFontMetrics(g2.getFont()).getHeight();
-
-        for (int w : g2.getFontMetrics().getWidths()) {
-            maxBound = Math.max(maxBound, w);
-        }
-
-        double fontSize = sqrSize / (maxBound * maxDigits) * g2.getFont().getSize();
-        g2.setFont(g2.getFont().deriveFont((float) fontSize));
-
         if (xSqrs > 0 && ySqrs > 0) {
+            //Calculates number of digits
+            int maxDigits = (int) Math.max(Math.floor(Math.log10(xSqrs)) + 1, Math.floor(Math.log(ySqrs - 1) / Math.log(26) + 1));
 
-            double fontY = g2.getFontMetrics().getMaxDescent() + g2.getFontMetrics().getLeading();
-            double fontX;
+            //Get max dimension of current font characters
+            double maxBound = g2.getFontMetrics(g2.getFont()).getHeight();
+            for (int w : g2.getFontMetrics().getWidths()) {
+                maxBound = Math.max(maxBound, w);
+            }
+
+            //Scale font according to space availible
+            double fontSize = sqrSize / (maxBound * maxDigits) * g2.getFont().getSize();
+            g2.setFont(g2.getFont().deriveFont((float) fontSize));
+
+            double xOffset = (getWidth() - 1 - (xSqrs + 1) * sqrSize) / 2;
+            double yOffset = (getHeight() - 1 - (ySqrs + 1) * sqrSize) / 2;
+
+            //Distance to bottom of text to placement position
+            double fontLineOffset = g2.getFontMetrics().getMaxDescent() + g2.getFontMetrics().getLeading();
 
             for (int x = 1; x <= xSqrs + 1; x++) {
-                if (x <= xSqrs) {
-                    fontX = g2.getFontMetrics().getStringBounds(Integer.toString(x), g2).getWidth();
+                double xSquareStart = x * sqrSize;
 
-                    g2.drawString(Integer.toString(x), (float) (x * sqrSize + (sqrSize - fontX) / 2), (float) (sqrSize - fontY));
+                if (x <= xSqrs) {
+                    double labelWidth = g2.getFontMetrics().getStringBounds(Integer.toString(x), g2).getWidth();
+
+                    double xRel = xSquareStart + (sqrSize - labelWidth) / 2;
+                    //Resting on grid
+                    double yRel = sqrSize - fontLineOffset;
+
+                    g2.drawString(Integer.toString(x), (float) (xRel + xOffset), (float) (yRel + yOffset));
                 }
-                Line2D.Double xGrid = new Line2D.Double(x * sqrSize, sqrSize, x * sqrSize, (ySqrs + 1) * sqrSize);
+                Line2D.Double xGrid = new Line2D.Double(xSquareStart + xOffset, sqrSize + yOffset, xSquareStart + xOffset, (ySqrs + 1) * sqrSize + yOffset);
                 g2.draw(xGrid);
             }
 
-            double fontYHeight = g2.getFontMetrics().getHeight();
+            //Total height of font
+            double fontHeight = g2.getFontMetrics().getHeight();
 
             for (int y = 1; y <= ySqrs + 1; y++) {
-                if (y <= ySqrs) {
-                    fontX = g2.getFontMetrics().getStringBounds(Integer.toString(y), g2).getWidth();
+                double ySquareStart = y * sqrSize;
 
-                    g2.drawString(Integer.toString(y), (float) ((sqrSize - fontX) / 2), (float) (sqrSize * (y + 1) - (sqrSize - fontYHeight) / 2 - fontY));
+                if (y <= ySqrs) {
+                    double fontX = g2.getFontMetrics().getStringBounds(Integer.toString(y), g2).getWidth();
+
+                    double xRel = (sqrSize - fontX) / 2;
+                    //Vertical Center
+                    double yRel = ySquareStart + sqrSize / 2 + fontHeight / 2 - fontLineOffset;
+
+                    g2.drawString(Integer.toString(y), (float) (xRel + xOffset), (float) (yRel + yOffset));
                 }
 
-                Line2D.Double xGrid = new Line2D.Double(sqrSize, y * sqrSize, (xSqrs + 1) * sqrSize, y * sqrSize);
-                g2.draw(xGrid);
+                Line2D.Double rowLine = new Line2D.Double(sqrSize + xOffset, ySquareStart + yOffset, (xSqrs + 1) * sqrSize + xOffset, ySquareStart + yOffset);
+                g2.draw(rowLine);
             }
         }
     }
