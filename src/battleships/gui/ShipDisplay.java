031bbb82c15f0cb7c2013ea425c028ee642ca394
Shen Yichen <2007.yichen@gmail.com>
2015-07-14 12:47:43 +0800

    Draw grid
    Grid is position to the center of the panel and is scaled to fit based on the squares and panel size.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- src/battleships/gui/ShipDisplay.java
+++ src/battleships/gui/ShipDisplay.java
@@ -8,6 +8,7 @@ package battleships.gui;
 import battleships.model.Square;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 import javax.swing.JPanel;
 
 /**
@@ -22,8 +23,8 @@ import javax.swing.JPanel;
  */
 public class ShipDisplay extends JPanel {
 
-    private int gridWidth;
-    private int gridHeight;
+    private int cols;
+    private int rows;
     private Iterable<Square> ship;
 
     /**
@@ -36,11 +37,31 @@ public class ShipDisplay extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
 
-        if (ship != null && gridHeight > 0 && gridWidth > 0) {
+        if (ship != null && rows > 0 && cols > 0) {
 
             final Graphics2D g2 = (Graphics2D) g.create();
 
             try {
+                //Draw grid
+                double sqrSize = Math.min((getWidth() - 1) * 1.0 / (cols + 1), (getHeight() - 1) * 1.0 / (rows + 1));
+
+                double xOffset = (getWidth() - sqrSize * cols) / 2;
+                double yOffset = (getHeight() - sqrSize * rows) / 2;
+
+                for (int x = 1; x <= cols; x++) {
+                    double xSquareStart = x * sqrSize;
+
+                    Line2D.Double xGrid = new Line2D.Double(xSquareStart + xOffset, sqrSize + yOffset, xSquareStart + xOffset, (rows + 1) * sqrSize + yOffset);
+                    g2.draw(xGrid);
+                }
+
+                for (int y = 1; y <= rows; y++) {
+                    double ySquareStart = y * sqrSize;
+
+                    Line2D.Double yGrid = new Line2D.Double(sqrSize + xOffset, ySquareStart + yOffset, (cols + 1) * sqrSize + xOffset, ySquareStart + yOffset);
+                    g2.draw(yGrid);
+                }
+                //End of draw grid
 
             } finally {
                 g2.dispose();
