e2b337c19766e1d8ea78be0b852624cd593a894b
Shen Yichen <2007.yichen@gmail.com>
2015-12-05 14:08:07 +0800

    Added package info for controllers
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ src/battleships/controller/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2015. Shen Yichen <2007.yichen@gmail.com>
+ * Under The MIT License.
+ */
+
+/**
+ * The GUI controller package.
+ * <p>
+ * This package contains controllers meant for the original GUI.
+ *
+ * @author Shen Yichen <2007.yichen@gmail.com>
+ * @since v1.0.0
+ */
+package battleships.controller;
